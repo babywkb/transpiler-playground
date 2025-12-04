8345173babcbe29f1aac9eee5ee394bc34c583db
babywkb <baby_hiroki_check@yahoo.co.jp>
2025-12-04 14:29:31 +0900

    format

--- babel-plugin/plugin.js
+++ babel-plugin/plugin.js
@@ -1,9 +1,9 @@
-module.exports = function({ types: t }) {
+module.exports = function ({ types: t }) {
   return {
     visitor: {
       FunctionDeclaration(path) {
         path.node.id.name = "prefix_" + path.node.id.name;
-      },
-    },
+      }
+    }
   };
 };
