1a8e51b88c583d0cc185fcdae0c73dd566b67d61
babywkb <baby_hiroki_check@yahoo.co.jp>
2025-12-08 16:15:24 +0900

    正規表現で対象を取りにくいお題に変更

--- babel-plugin/input/input.js
+++ babel-plugin/input/input.js
@@ -1,3 +1,6 @@
-function hello() {
-  console.log("Hello, world!");
+function calcArray() {
+  const arr = [1, 2, 3];
+  console.log("value", arr.map(x => x * 2).join(","));
+  const result = arr.filter(n => n > 1);
+  console.log("done", result);
 }
