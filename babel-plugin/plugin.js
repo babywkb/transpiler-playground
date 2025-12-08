12f201b087dc3b3e23c611baf26143f3d451f2f1
babywkb <baby_hiroki_check@yahoo.co.jp>
2025-12-08 16:18:05 +0900

    helper-pluginとtypesを利用した処理に変更

--- babel-plugin/plugin.js
+++ babel-plugin/plugin.js
@@ -1,9 +1,24 @@
+import { declare } from "@babel/helper-plugin-utils";
+import * as t from "@babel/types";
+
 export default declare((api, options) => {
+  api.assertVersion(7);
+
   return {
+    name: "remove-console-log",
     visitor: {
-      FunctionDeclaration(path) {
-        path.node.id.name = "prefix_" + path.node.id.name;
-      }
-    }
+      CallExpression(path) {
+        const { node } = path;
+
+        // console.log(...) を検出
+        if (
+          t.isMemberExpression(node.callee) &&
+          t.isIdentifier(node.callee.object, { name: "console" }) &&
+          t.isIdentifier(node.callee.property, { name: "log" })
+        ) {
+          path.remove(); // その呼び出しを削除
+        }
+      },
+    },
   };
 });
